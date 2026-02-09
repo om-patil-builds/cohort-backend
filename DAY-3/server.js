4b273ef982fc878fb9fd55e11517b44a5ed43844
om-patil-Builds <vp8843o3@gmail.com>
2026-02-09 21:53:44 +0530

    second commit

--- DAY-3/server.js
+++ DAY-3/server.js
@@ -1,20 +1,19 @@
 const express = require('express')
-
 const server = express()
 
 server.use(express.json())
-
 const notes = []
-
-server.post('/notes', (req,res)=>{
-    console.log(req.body)
-    res.send("notes Recieved")
+server.post('/notes', (req, res) => {
+ console.log(req.body)
+ notes.push(req.body)
+ res.send('notes created')
 })
 
-server.get('/notes' , (req,res)=>{
-    res.send("Data Takes Sucsessfully")
+server.get('/notes',(req,res)=>{
+    res.send(notes)
 })
 
-server.listen(3000,()=>{
-    console.log("server is running on port 3000")
+server.listen(3000, () => {
+
+  console.log('server running on 3000 port')
 })
